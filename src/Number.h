dc345934dc9b8811f9de8b1774e2f27680ddf3aa
olivan139 <62103001+olivan139@users.noreply.github.com>
2020-04-27 19:17:20 +0300

    Add files via upload

---
+++ src/Number.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+class Number
+{
+
+public:
+
+	Number();
+	Number(const std::string& a);
+	Number(const Number& a);
+	~Number();
+
+	std::pair<Number, Number> split(int n) const;
+
+	Number& adding_zeros_left(const size_t& n);
+	Number& adding_zeros_right(const size_t& n);
+
+	Number& push_front(const char& el);
+	Number& push_back(const char& el);
+
+	Number operator +(const Number& scnd) const;
+	Number operator - (const Number& numb2) const;
+	Number operator *(const Number& scnd) const;
+
+	size_t size() const;
+
+	char operator [] (size_t i) const;
+	char& operator [] (size_t i);
+
+	std::string& getStr();
+	std::string getStr() const;
+
+
+	friend std::ostream& operator << (std::ostream& out, const Number& res);
+	friend bool operator == (const Number& a, const Number& b);
+
+private:
+
+	std::string dg;
+	static std::vector<std::vector<std::string>> table;
+};
